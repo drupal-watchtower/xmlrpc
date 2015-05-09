86810c13b484986fc69a606c4712218a8c87230f
Andy Postnikov <apostnikov@gmail.com>
2015-05-10 02:03:43 +0530

    Fix tests

--- src/Tests/XmlRpcMessagesTest.php
+++ src/Tests/XmlRpcMessagesTest.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Definition of Drupal\xmlrpc\Tests\XmlRpcMessagesTest.
+ * Contains \Drupal\xmlrpc\Tests\XmlRpcMessagesTest.
  */
 
 namespace Drupal\xmlrpc\Tests;
@@ -26,29 +26,29 @@ class XmlRpcMessagesTest extends WebTestBase {
   /**
    * Make sure that XML-RPC can transfer large messages.
    */
-  function testSizedMessages() {
-    $xml_url = url('xmlrpc.php', array('absolute' => TRUE));
-    $sizes = array(8, 80, 160);
+  public function testSizedMessages() {
+    $xml_url = \Drupal::url('xmlrpc.php', [], ['absolute' => TRUE]);
+    $sizes = [8, 80, 160];
     foreach ($sizes as $size) {
       $xml_message_l = xmlrpc_test_message_sized_in_kb($size);
-      $xml_message_r = xmlrpc($xml_url, array('messages.messageSizedInKB' => array($size)));
+      $xml_message_r = xmlrpc($xml_url, ['messages.messageSizedInKB' => [$size]]);
 
-      $this->assertEqual($xml_message_l, $xml_message_r, format_string('XML-RPC messages.messageSizedInKB of %s Kb size received', array('%s' => $size)));
+      $this->assertEqual($xml_message_l, $xml_message_r, format_string('XML-RPC messages.messageSizedInKB of %s Kb size received', ['%s' => $size]));
     }
   }
 
   /**
    * Ensure that hook_xmlrpc_alter() can hide even builtin methods.
    */
-  protected function testAlterListMethods() {
+  public function testAlterListMethods() {
     // Ensure xmlrpc_test.alter() is disabled and retrieve regular list of methods.
     \Drupal::state()->set('xmlrpc_test.alter', FALSE);
-    $url = url('xmlrpc.php', array('absolute' => TRUE));
-    $methods1 = xmlrpc($url, array('system.listMethods' => array()));
+    $url = \Drupal::url('xmlrpc.php', [], ['absolute' => TRUE]);
+    $methods1 = xmlrpc($url, ['system.listMethods' => []]);
 
     // Enable the alter hook and retrieve the list of methods again.
     \Drupal::state()->set('xmlrpc_test.alter', TRUE);
-    $methods2 = xmlrpc($url, array('system.listMethods' => array()));
+    $methods2 = xmlrpc($url, ['system.listMethods' => []]);
 
     $diff = array_diff($methods1, $methods2);
     $this->assertTrue(is_array($diff) && !empty($diff), 'Method list is altered by hook_xmlrpc_alter');
